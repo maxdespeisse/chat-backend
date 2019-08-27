cad5aff611a03d21d0fbf2f212b6fbdd26318482
Maxime Despeisse <maxime.despeisse@gmail.com>
2019-08-27 11:49:21 +0200

    Init app

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.send('<h1>Hello world!</h1>');
 });
+
+let messages = [];
 
 io.on('connection', socket => {
   console.log('a user connected');
+
+  socket.emit('news', { messages });
+
+  socket.on('chat message', message => {
+    console.log(`New message ${message}`);
+    messages.push(message);
+    io.emit('chat message', message);
+  });
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 })
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
 });
